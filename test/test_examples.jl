99215c0d635cf8b04e4bb4c0d6b0f0a55ab11178
Julian Straus <104911227+JulStraus@users.noreply.github.com>
2024-03-03 12:02:46 +0100

    Changed examples to be consistent with EMB (#11)

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -1,7 +1,6 @@
 
 @testset "Run examples" begin
     exdir = joinpath(@__DIR__, "../examples")
-
     files = first(walkdir(exdir))[3]
     for file ∈ files
         if splitext(file)[2] == ".jl"
@@ -13,9 +12,5 @@
             end
         end
     end
-
-    # Cleanup the test environment. Remove EnergyModelsRenewableProducers from
-    # the environment, since it is added with `Pkg.develop` by the examples. The
-    # tests can not be run with with the package in the environment.
-    Pkg.rm("EnergyModelsRenewableProducers")
+    Pkg.activate(@__DIR__)
 end
