75296fb04e7fa5739605f81dacb3faef1ba366af
Lars Hellemo <Lars.Hellemo@sintef.no>
2024-06-17 16:47:34 +0200

    Use dev version when testing examples Do not print results when testing

--- examples/simple_nondisres.jl
+++ examples/simple_nondisres.jl
@@ -1,6 +1,8 @@
 using Pkg
 # Activate the local environment including EnergyModelsRenewableProducers, HiGHS, PrettyTables
 Pkg.activate(@__DIR__)
+# Use dev version if run as part of tests
+haskey(ENV, "EMX_TEST") && Pkg.develop(path=joinpath(@__DIR__,".."))
 # Install the dependencies.
 Pkg.instantiate()
 
@@ -98,19 +100,21 @@ optimizer = optimizer_with_attributes(HiGHS.Optimizer, MOI.Silent() => true)
 m = EMB.run_model(case, model, optimizer)
 
 # Display some results
-@info "Curtailment of the wind power source"
-pretty_table(
-    JuMP.Containers.rowtable(
-        value,
-        m[:curtailment];
-        header = [:Node, :TimePeriod, :Curtailment],
-    ),
-)
-@info "Capacity usage of the power source"
-pretty_table(
-    JuMP.Containers.rowtable(
-        value,
-        m[:cap_use][case[:nodes][1],:];
-        header = [:TimePeriod, :Usage],
-    ),
-)
+if !haskey(ENV, "EMX_TEST")
+    @info "Curtailment of the wind power source"
+    pretty_table(
+        JuMP.Containers.rowtable(
+            value,
+            m[:curtailment];
+            header = [:Node, :TimePeriod, :Curtailment],
+        ),
+    )
+    @info "Capacity usage of the power source"
+    pretty_table(
+        JuMP.Containers.rowtable(
+            value,
+            m[:cap_use][case[:nodes][1],:];
+            header = [:TimePeriod, :Usage],
+        ),
+    )
+end
