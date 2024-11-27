90dd1e362ebf639613919e242777e17624b9ac98
Julian Straus <julian.straus@sintef.no>
2024-11-27 11:58:32 +0100

    Reactivated the other tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,29 +12,29 @@ const EMRP = EnergyModelsRenewableProducers
 @testset "RenewableProducers" begin
     include("utils.jl")
 
-    # @testset "RP | Non-dispatchable renewable energy source" begin
-    #     include("test_nondisres.jl")
-    # end
+    @testset "RP | Non-dispatchable renewable energy source" begin
+        include("test_nondisres.jl")
+    end
 
-    # @testset "RP | Hydropower" begin
-    #     include("test_hydro.jl")
-    # end
+    @testset "RP | Hydropower" begin
+        include("test_hydro.jl")
+    end
 
-    # @testset "RP | Detailed hydropower" begin
-    #     include("test_detailed_hydro.jl")
-    # end
+    @testset "RP | Detailed hydropower" begin
+        include("test_detailed_hydro.jl")
+    end
 
     @testset "RP | Battery storage" begin
         include("test_battery.jl")
     end
 
-    # @testset "RP | examples" begin
-    #     include("test_examples.jl")
-    # end
+    @testset "RP | examples" begin
+        include("test_examples.jl")
+    end
 
-    # redirect_stdio(stdout=devnull, stderr=devnull) do
-    #     @testset "RP | constructors" begin
-    #         include("test_constructors.jl")
-    #     end
-    # end
+    redirect_stdio(stdout=devnull, stderr=devnull) do
+        @testset "RP | constructors" begin
+            include("test_constructors.jl")
+        end
+    end
 end
