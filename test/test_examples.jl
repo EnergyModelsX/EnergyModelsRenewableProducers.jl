f5a7d299c05dc385dcfa93b67424e2faebe88262
Espen Flo Bødal <espen.bodal@sintef.no>
2025-10-23 15:29:45 +0200

    Updated test_examples

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -1,15 +1,23 @@
-ENV["EMX_TEST"] = true # Set flag for example scripts to check if they are run as part of the tests
+@testitem "Examples" begin
+    using Logging
+    # Get the global logger and set the loglevel to Warn
+    logger_org = global_logger()
+    logger_new = ConsoleLogger(Warn)
+    global_logger(logger_new)
 
-@testset "Run examples" begin
+    ENV["EMX_TEST"] = true # Set flag for example scripts to check if they are run as part of the tests
     exdir = joinpath(@__DIR__, "..", "examples")
     files = filter(endswith(".jl"), readdir(exdir))
     for file ∈ files
         @testset "Example $file" begin
-            redirect_stdio(stdout=devnull, stderr=devnull) do
+            redirect_stdio(stdout = devnull) do
                 include(joinpath(exdir, file))
-                @test termination_status(m) == MOI.OPTIMAL
             end
+            @test termination_status(m) == MOI.OPTIMAL
         end
     end
     Pkg.activate(@__DIR__)
+
+    # Reset the loglevel
+    global_logger(logger_org)
 end
